be87e6deb709e4708d5b4fbb3ebcd83ae19d0032
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-24 19:58:15 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -9,7 +9,7 @@ from WebStreamer.bot import StreamBot, logger
 from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.file_id import file_id
+from pyrogram.file_id import FileId
 
 @StreamBot.on_message(
     filters.private
@@ -25,7 +25,7 @@ from pyrogram.file_id import file_id
     ),
     group=4,
 )
-async def media_receive_handler(_, m: Message):
+async def media_receive_handler(_, file_id: FileId, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
         return await m.reply("You are not <b>allowed to use</b> this <a href='https://github.com/EverythingSuckz/TG-FileStreamBot'>bot</a>.", quote=True)
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
