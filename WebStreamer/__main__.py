29de5279ce54fdd1f3428178f81f57739e5ca06d
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-02-25 07:48:28 +0300

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -63,3 +63,7 @@ if __name__ == "__main__":
         pass
     except Exception as err:
         logging.error(err.with_traceback(None))
+    finally:
+        loop.run_until_complete(cleanup())
+        loop.stop()
+        logging.info("Stopped Services")
