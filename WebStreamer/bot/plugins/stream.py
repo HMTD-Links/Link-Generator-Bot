d203f29e14ca5386cfc2d12a85f46a7cc3469dd3
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-25 07:30:58 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -36,8 +36,8 @@ async def media_receive_handler(_, m: Message):
     logger.info(f"Generated link :- {stream_link} for {m.from_user.first_name}")
     try:
         await m.reply_cap(
-           cap = m.reply(f"<b>{}\n➠ Link :- [Click here]({})</b>\n<b>(<a href='{}'>Shortened</a>)</b>").format(
-                file_caption, stream_link, short_link
+            cap=m.reply(f"<b>{}\n➠ Link :- [Click here]({})</b>\n<b>(<a href='{}'>Shortened</a>)</b>").format(
+                cap, stream_link, short_link
             ),
             quote=True,
             parse_mode=ParseMode.HTML,
