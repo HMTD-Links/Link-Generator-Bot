baacd1fce58cd860b45fed1ecbc3b767846a998b
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-25 10:58:23 +0530

    Update translation.py

--- WebStreamer/translation.py
+++ WebStreamer/translation.py
@@ -21,15 +21,6 @@ Example :- Our Bot's Commend and Links Tutorial
 📡 Hosted on : VPS
 📢 Update Channel : [Star Movies Tamil](https://t.me/Star_Moviess_Tamil)</b>"""
 
-      # Get Movies with Links 👇🏻
-      MOVIES = """<b>Hi 👋🏻 {mention} ♥️,
-You Can Get Movies with Links 🔗. Our Bot will Support 4GB Files 📂. But We're Always Not Upload PreDVD Movies and HDRip Movies + HC-ESub Movies. Because it Only Lasts for a Few Days. Maybe Because I Upload PreDVD Until Bluray or TRUE WEB-DL Comes out Value for This Movie. So We Will Not Upload this Bot. Don't worry we will upload it on Star Movies Tamil's PreDVD Movies Channel. Similarly, HDRip + HC-ESub Movies and Bluray or TRUE WEB-DL are the Value for This Movie. We Will Upload on Our Star Movies Tamil's Channel like So PreDVD Movies.
-🗣️ Explanation :-
-PreDVD :- Theatre Print With HQ Clean Audio.
-HC-ESub (Hard Coded English Subtitle) :- In This Movies, English Subtitle is Attached to HDRip 🎥 Video and We Can't Remove it
-❗Note :- We're Recommend Brave Browser
-Request Your Movies 🎥 in Our Admin :- [👨🏻‍✈️ Karthik](https://t.me/Star_Movies_Karthik)</b>"""
-
       #Support Text
       SUPPORT = """<b>Hi 👋🏻 {mention} ♥️,
 I'm Star Movies Official Feedback Bot. You Want Any Help or Of You Need Any Movie Links Massage us Me and Get Solution to Admins</b>"""
