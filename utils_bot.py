f9a3f2f4b1af0cbd15503390701a1ecc3c6debc6
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-26 08:03:43 +0530

    Create utils_bot.py

---
+++ utils_bot.py
@@ -0,0 +1,40 @@
+import logging
+import os
+import threading
+import time
+from asyncio import TimeoutError
+from pyrogram import filters
+
+LOGGER = logging.getLogger(__name__)
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self):
+        nextTime = time.time() + self.interval
+        while not self.stopEvent.wait(nextTime - time.time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
+
+
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+
