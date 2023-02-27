4159809237e8900ae3ecc1531a3966442db9b5cf
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-27 07:29:59 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -3,7 +3,7 @@ from pyrogram import filters, errors
 from WebStreamer.vars import Var
 from urllib.parse import quote_plus
 from WebStreamer.utils import get_hash, get_name
-from WebStreamer.utils.file_properties import get_name, get_media_file_cap, get_media_file_size, get_media_from_message
+from WebStreamer.utils.file_properties import get_name, get_media_file_caption, get_media_file_size, get_media_from_message
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.file_id import FileId
@@ -48,19 +48,19 @@ async def multi_files(bot, msg):
 )
 async def media_receive_handler(_, m: Message):
     if Var.ALLOWED_USERS and not ((str(m.from_user.id) in Var.ALLOWED_USERS) or (m.from_user.username in Var.ALLOWED_USERS)):
-        return await m.reply("You are not <b>allowed to use</b> this <a href='https://github.com/EverythingSuckz/TG-FileStreamBot'>bot</a>.", quote=True)
+        return await m.reply("**You are not <b>allowed to use</b> this <a href='https://github.com/EverythingSuckz/TG-FileStreamBot'>bot</a>.**", quote=True)
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
-    cap = get_media_file_cap(log_msg)
+    file_caption = get_media_file_caption(log_msg)
     file_name = get_name(log_msg)
     file_size = humanbytes(get_media_file_size(m))
     logging.info(f"Generated link :- {stream_link} for {m.from_user.first_name}")
     try:
         await m.reply_text(
-            text="<b>{} {} {}\n➠ Link :- [Click here]({})</b>\n<b>(<a href='{}'>Shortened</a>)</b>".format(
-                file_name, file_size, cap, stream_link, short_link
+            text="<b>File Name :-{}\nFile Size :-{}\nFile Captain :-{}\n➠ Link :- [Click here]({})</b>\n<b>(<a href='{}'>Shortened</a>)</b>".format(
+                file_name, file_size, file_caption, stream_link, short_link
             ),
             quote=True,
             parse_mode=ParseMode.HTML,
@@ -71,7 +71,7 @@ async def media_receive_handler(_, m: Message):
     except errors.ButtonUrlInvalid:
         await m.reply_text(
             text="<b>{}\n➠ Link :- [Click here]({})</b>\n\nshortened: {})".format(
-                cap, stream_link, short_link
+                file_caption, stream_link, short_link
             ),
             quote=True,
             parse_mode=ParseMode.HTML,
