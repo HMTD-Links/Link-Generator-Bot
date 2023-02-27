6be16e742fb8f9384eb7aedb2946a5e41035d295
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-27 17:13:27 +0530

    Update __init__.py

--- WebStreamer/utils/__init__.py
+++ WebStreamer/utils/__init__.py
@@ -1,4 +1,4 @@
 from .keepalive import ping_server
 from .time_format import get_readable_time
-from .file_properties import get_hash, get_name, get_media_file_caption
+from .file_properties import get_hash, get_name
 from .custom_dl import ByteStreamer
