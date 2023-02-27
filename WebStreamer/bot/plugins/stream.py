2193eab4b21b0ac262c820f6fe83a3bd7162321c
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-27 07:02:01 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -53,7 +53,7 @@ async def media_receive_handler(_, m: Message):
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
-    cap = get_media_file_cap(log_msg)
+    cap = get_media_from_message(log_msg)
     file_name = get_name(log_msg)
     file_size = humanbytes(get_media_file_size(m))
     logging.info(f"Generated link :- {stream_link} for {m.from_user.first_name}")
