56ce105097dad90c8df6105ad4bd7e03332d9bd8
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-27 20:19:42 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -35,8 +35,8 @@ class Var(object):
     DEBUG = str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
     USE_SESSION_FILE = str(environ.get("USE_SESSION_FILE", "0").lower()) in ("1", "true", "t", "yes", "y")
     ALLOWED_USERS = [x.strip("@ ") for x in str(environ.get("ALLOWED_USERS", "1391556668 5162208212") or "").split(",") if x.strip("@ ")]
-DB_URL = environ.get('DATABASE_URI', "mongodb+srv://karthikgaming:KarthikUK007@cluster0.ach7nle.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = environ.get('DATABASE_NAME', "Cluster0")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001342411240"))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1391556668").split())
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+    DB_URL = environ.get('DATABASE_URI', "mongodb+srv://karthikgaming:KarthikUK007@cluster0.ach7nle.mongodb.net/?retryWrites=true&w=majority")
+    DB_NAME = environ.get('DATABASE_NAME', "Cluster0")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001342411240"))
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1391556668").split())
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
