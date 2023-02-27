7ccade1a5718837ada5bf33ff679f8b5e1877ef6
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-27 07:30:56 +0530

    Update __init__.py

--- WebStreamer/utils/__init__.py
+++ WebStreamer/utils/__init__.py
@@ -1,7 +1,4 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
-
 from .keepalive import ping_server
 from .time_format import get_readable_time
-from .file_properties import get_hash, get_name, get_media_file_cap
+from .file_properties import get_hash, get_name, get_media_file_caption
 from .custom_dl import ByteStreamer
