c68c6f13223e08278f57cf6ffed8e887d5a54677
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-02-27 18:27:56 +0300

    Update check_user.py

--- WebStreamer/handlers/check_user.py
+++ WebStreamer/handlers/check_user.py
@@ -27,6 +27,7 @@ async def handle_user_status(bot, cmd):
 
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
+        print(ban_status)
         if (
             datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
         ).days > ban_status["ban_duration"]:
